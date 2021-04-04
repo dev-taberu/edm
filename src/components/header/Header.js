332983de4e88c237e3794bf4cfb9886c18a125ff
Taberu <dev.taberu@gmail.com>
2021-04-04 14:33:59 +0300

    some changes

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import { Button, Navbar } from "react-bootstrap"
+
+export default () => {
+    return (
+        <header>
+            <Navbar bg='dark'>
+                <Button type='dark'>Hello</Button>
+            </Navbar>
+        </header>
+    )
+}
